af1108cec82cfd0c63a0fcf8c134682fb606d3cf
Guide0n <alvarogsllegos@gmail.com>
2025-09-24 22:20:47 -0600

    Subiendo Cuenta Bancaria

---
+++ CuentaBancaria/CuentaBancaria/Cuenta1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentaBancaria
+    {
+        internal class Cuenta1
+        {
+            string num_cuenta;
+            string nombre_cli;
+            float saldo = 0;
+
+            public Cuenta1(string num_cuenta, string nombre_cli, float saldo)
+            {
+                this.num_cuenta = num_cuenta;
+                this.nombre_cli = nombre_cli;
+                this.saldo = saldo;
+            }
+
+            public void getSaldo()
+            {
+                Console.WriteLine("Su saldo es: $" + saldo);
+            }
+            public void depositar(float cantidad)
+            {
+
+                if (saldo > 0)
+                {
+                    saldo += cantidad;
+                    Console.WriteLine("Nuevo saldo : $" + saldo);
+                }
+                else
+                {
+                    Console.WriteLine("El monto debe de ser mayor a $0");
+                }
+            }
+
+            public void retirar(float cantidad)
+            {
+                if (saldo > cantidad)
+                {
+                    saldo -= cantidad;
+                    Console.WriteLine("Su nuevo saldo: $" + saldo);
+                }
+                else { Console.WriteLine("La cantidad a retirar es mayor que el saldo a favor."); }
+
+            }
+        }
+    }
+}
+
---
+++ CuentaBancaria/CuentaBancaria/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==MENU DE OPERACIONES==");
+            Console.WriteLine("1. CONSULTAR SALDO");
+            Console.WriteLine("2. DEPOSITAR");
+            Console.WriteLine("3. RETIRAR");
+            Console.WriteLine("INGRSE UNA OPCION: ");
+            int opc;
+            opc = int.Parse(Console.ReadLine());
+
+            Cuenta1 miCuenta = new Cuenta1("24150030", "Alvaro Gallegos", 5000);
+
+            switch (opc)
+            {
+                case 1: miCuenta.getSaldo(); break;
+                case 2:
+                    Console.WriteLine("INGRESE EL MONTO: ");
+                    int cant = int.Parse(Console.ReadLine());
+                    miCuenta.depositar(cant);
+                    break;
+                case 3:
+                    Console.WriteLine("INGRESE EL MONTO: ");
+                    int canti = int.Parse(Console.ReadLine());
+                    miCuenta.retirar(canti);
+                    break;
+
+            }
+
+
+        }
+    }
+}
+
+
